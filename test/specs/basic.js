11a6b8e27bcf93efecaad61255d245c2a89fc461
dfavo <favorskyd@gmail.com>
2019-09-22 11:40:01 -0700

    Add new test to test file

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,9 +1,17 @@
 const assert = require('assert')
 
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io')
-        const title = browser.getTitle()
-        assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
-    })
-})
+describe('Main page', () => {
+
+ //   it('should have the right title', () => {
+//      browser.url('https://webdriver.io')
+ //       const title = browser.getTitle()
+//     assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
+
+it ('should have correct site name', () => {
+    browser.url('https://app.pasv.us/');
+    const element = $('//span[@id="site-name"]');
+    const text = element.getText();
+    assert.strictEqual(text, 'Progress Monitor');
+        
+    });
+});
