c10bc940fdb69694b6d9b83eea247afcfb51f093
dfavo <favorskyd@gmail.com>
2019-09-22 18:10:28 -0700

    Add six files

---
+++ test/actions/loginAction.js
@@ -0,0 +1,20 @@
+function loginAction(browser){
+  browser.url('https://stage.pasv.us/user/login/');
+  const emailField = $('//input[@name="email"]');
+    const passwordField = $('//input[@name="password"]');
+    const loginButton = $('//button[@type="submit"]')
+
+    const EMAIL = 'admin@test.com';
+    const PASSWORD = '11111';
+
+    emailField.setValue(EMAIL);
+    passwordField.setValue(PASSWORD);
+
+
+    button.click();
+    browser.pause(10000);
+
+}
+
+module.exports = loginAction;
+  
