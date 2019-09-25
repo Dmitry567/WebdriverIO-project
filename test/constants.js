de838af4cd78f02ebc1386e80e6dfc4199f75b9a
dfavo <favorskyd@gmail.com>
2019-09-24 22:26:57 -0700

    Add file

--- test/constants.js
+++ test/constants.js
@@ -1,3 +1,25 @@
 const baseUrl = 'https://stage.pasv.us';
 
-module.exports = {baseUrl};
+const url = {
+  loginUrl = `${baseUrl}/user/login`,
+};
+
+const user = {
+  admin: {
+    login:'admin@test.com',
+    password: '11111'
+    
+  },
+  student: {
+    login: '',
+    password: ''
+  }
+};
+
+
+
+
+module.exports = {
+  url,
+  user
+};
