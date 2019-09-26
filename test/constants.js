737644ab33edabcc44095a448cd8c7c16672489f
dfavo <favorskyd@gmail.com>
2019-09-25 23:14:57 -0700

    Add two files

--- test/constants.js
+++ test/constants.js
@@ -1,19 +1,31 @@
-const baseUrl = 'https://stage.pasv.us';
+const baseUrl = 'https://server--stage.pasv.us';
 
 const url = {
+  baseUrl,
   loginUrl = `${baseUrl}/user/login`,
+  registerUrl: `${baseUrl}/user/register`,
+  serverUrl: 'https://server--stage.pasv.us'
 };
 
 const user = {
   admin: {
-    login:'admin@test.com',
-    password: '11111'
+    email:'admin@test.com',
+    password: '11111',
+    id: '5d687e4983d5d600380e6b51',
+    name: 'Ruslan Admin',
+    phone: '+12312312334'
     
   },
   student: {
-    login: '',
-    password: ''
+    email: 'student_dmitry@test.com',
+    password: '11111',
+    // id:'5d687e4983d5d600380e6b51',
+    name: 'Test Student',
+    phone: '+12312312334',
+    about: 'About Test Student About Test Student About Test Student About Test Student About Test Student',
+    goals: 'About Test Student About Test Student About Test Student About Test Student About Test Student' 
   }
+
 };
 
 
